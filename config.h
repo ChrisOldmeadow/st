e4b1f344196dab0edcb1770e0618593bca061e50
Chris Oldmeadow <chris.oldmeadow@protonmail.com>
2020-08-22 11:55:49 +1000

    added joypixels for font2

--- config.h
+++ config.h
@@ -8,7 +8,7 @@
 static char *font = "Mononoki Nerd Font:pixelsize=14:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", 
+	"JoyPixels:pixelsize=10:antialias=true:autohint=true",
 	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", 
 };
 static int borderpx = 2;
